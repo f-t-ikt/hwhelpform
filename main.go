e07ff17951fb1265c1a654bf2dcfb829de1a250d
home <balsamicvinegar.tsukasalove@gmail.com>
2020-06-20 16:36:06 +0900

    create post.go

--- main.go
+++ main.go
@@ -20,13 +20,6 @@ var upgrader = websocket.Upgrader{}
 
 var helpList, callList = NewIdList(), NewIdList()
 
-// クライアントからは JSON 形式で受け取る 
-type Post struct {
-    // Message string `json:message`
-    Method string `json:Method`
-    Id     int    `json:Id`
-}
-
 // クライアントのハンドラ
 func HandleClients(w http.ResponseWriter, r *http.Request) {
     // websocket の状態を更新
@@ -89,115 +82,5 @@ func main() {
     if err != nil {
         log.Fatal("error starting http server::", err)
         return
-    }
-}
-
-func initialBroadcast(client *websocket.Conn) {
-    helpList.Each(func(v interface{}) bool {
-        post := Post {
-            Method: "help",
-            Id: v.(int),
-        }
-        err := client.WriteJSON(post)
-        if err != nil {
-            log.Printf("error occurred while writing post to client: %v", err)
-            client.Close()
-            clients.Delete(client)
-            return false
-        }
-        return true
-    })
-    
-    if _, ok := clients.Load(client); !ok {
-        return
-    }
-    
-    callList.Each(func(v interface{}) bool {
-        post := Post {
-            Method: "call",
-            Id: v.(int),
-        }
-        err := client.WriteJSON(post)
-        if err != nil {
-            log.Printf("error occurred while writing post to client: %v", err)
-            client.Close()
-            clients.Delete(client)
-            return false
-        }
-        return true
-    })
-}
-
-func procHelp(post *Post) {
-    id := post.Id
-    if helpList.Contains(id) {
-        return
-    }
-    
-    if callList.Contains(id) {
-        callList.Remove(id)
-        del := Post {
-            Method: "deleteCall",
-            Id: id,
-        }
-        broadcast <- del
-    }
-    
-    helpList.Add(id)
-    broadcast <- *post
-}
-
-func procCall(post *Post) {
-    id := post.Id
-    if callList.Contains(id) {
-        return
-    }
-    
-    if helpList.Contains(id) {
-        helpList.Remove(id)
-        del := Post {
-            Method: "deleteHelp",
-            Id: id,
-        }
-        broadcast <- del
-    }
-    
-    callList.Add(id)
-    broadcast <- *post
-}
-
-func procDeleteHelp(post *Post) {
-    helpList.Remove(post.Id)
-    broadcast <- *post
-}
-
-func procDeleteCall(post *Post) {
-    callList.Remove(post.Id)
-    broadcast <- *post
-}
-
-func broadcastPostsToClients() {
-    for {
-        // メッセージ受け取り
-        post := <-broadcast
-        // クライアントの数だけループ
-        // for client := range clients {
-        //　書き込む
-            // err := client.WriteJSON(post)
-            // if err != nil {
-                // log.Printf("error occurred while writing post to client: %v", err)
-                // client.Close()
-                // delete(clients, client)
-            // }
-        // }
-        clients.Range(func(client, stored interface{})bool {
-            err := client.(*websocket.Conn).WriteJSON(post)
-            if err != nil {
-                log.Printf("error occurred while writing post to client: %v", err)
-                client.(*websocket.Conn).Close()
-                clients.Delete(client)
-            }
-            return true
-        })
     }
 }
